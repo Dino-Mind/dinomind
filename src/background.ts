6ea55e0ad0daffa70ec4e322d7a9c65d07db3033
Bhdrpkcn <bhdrpkcn@gmail.com>
2024-12-04 02:35:09 +0300

    openSidePanel fix try #1

--- src/background.ts
+++ src/background.ts
@@ -9,11 +9,43 @@ import {
   saveChatHistory,
 } from "./redux/slices/sidePanelSlice";
 
+// Redux setup
 const store = configureStore({
   reducer: rootReducer,
 });
 const wrapStore = createWrapStore();
 wrapStore(store);
+
+// Handle toolbar icon click
+chrome.action.onClicked.addListener(() => {
+  chrome.runtime.sendMessage({ action: "openSidePanel" });
+
+  chrome.sidePanel.setOptions({ path: "sidepanel.html", enabled: true });
+
+  chrome.sidePanel
+    .setPanelBehavior({ openPanelOnActionClick: true })
+    .catch((error) => console.error(error));
+});
+
+// Handle messages for opening/closing the side panel
+chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
+  if (message.action === "openSidePanel") {
+    chrome.sidePanel.setOptions({ path: "sidepanel.html", enabled: true });
+
+    chrome.sidePanel
+      .setPanelBehavior({ openPanelOnActionClick: true })
+      .catch((error) => console.error(error));
+
+    // chrome.sidePanel.open();
+    store.dispatch(openSidePanel());
+    sendResponse({ status: "success", isOpen: true });
+  } else if (message.action === "closeSidePanel") {
+    chrome.sidePanel.setOptions({ enabled: false });
+    store.dispatch(saveChatHistory());
+    store.dispatch(closeSidePanel());
+    sendResponse({ status: "success", isOpen: false });
+  }
+});
 
 let currentTabId: number | null = null;
 
