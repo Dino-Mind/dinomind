5d73bfb7a2bcece08d51de9f242b1fb46fe7ac88
orabazu <ozerorhun@gmail.com>
2024-12-04 04:42:39 +0300

    feat: add useState import to CardContent component

--- src/components/ui/CardContent.tsx
+++ src/components/ui/CardContent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 // import ReactMarkdown from "react-markdown";
 import ContentChat from "./ContentChat";
 // import { ActionButtons } from "./ActionButtons";
